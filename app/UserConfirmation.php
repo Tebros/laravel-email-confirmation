d7e43eaa08f0616779a8b1e8f70e7e980d34de16
Tebros <info.tebros@web.de>
2018-01-14 22:17:24 +0100

    Added composer file

---
+++ app/UserConfirmation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserConfirmation extends Model
+{
+    protected $table = 'users_confirmation';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'token'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
